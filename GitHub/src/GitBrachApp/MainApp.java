65b6d3ab15b39a1bffe8758c689133c6c1e1f93e
mimi <lisa48250@gmail.com>
2022-06-03 09:57:57 +0800

    Add Nath Branch

--- GitHub/src/GitBrachApp/MainApp.java
+++ GitHub/src/GitBrachApp/MainApp.java
@@ -5,6 +5,14 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(10,5));
 	}
 
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
